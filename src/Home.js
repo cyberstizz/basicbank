a7570ad38dd7214e95aea0c6a8df1f2a7f905cac
Cyberstizz <charles.lamb.dev@gmail.com>
2023-01-29 15:06:20 -0500

    centered hero section

--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,23 @@ import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            
-        <Header>
-            Basic bank
-        </Header>
+        <div id="fullpage">
+
+
+        <div class="hero">
+         Basic bank
+        </div>
+
+        <div id="midsection"></div>
+        
+        <div class="accountsection"></div>
+        <div class="menusection"></div>
+
+
+        <div class="footer"></div>
+
+
+
 
         </div>
     )
