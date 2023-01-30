c3c8936d14c6f832d6918f1ba6bd2a6e7b8df9ec
Cyberstizz <charles.lamb.dev@gmail.com>
2023-01-30 08:18:52 -0500

    added sign and and create account buttons

--- src/Home.js
+++ src/Home.js
@@ -14,19 +14,30 @@ const Home = () => {
         Accounts
         <div class="accountsection">
         <ul class ="uselessList">
-        <li>chicken</li>
-        <li>cupcakes</li>
-        <li>pears</li>
-        <li>mangos</li>
-        <li>carraot cake</li>
+        <li><button>chicken</button></li>
+        <li><button>cupcakes</button></li>
+        <li><button>pears</button></li>
+        <li><button>mangos</button></li>
+        <li><button>carraot cake</button></li>
 
 
         </ul>
 
         </div>
-        <div class="infosection"></div>
+        <div class="infosection">
         Welcome to basic bank, the only place where 
         everyone can see your account and there is no privacy!!!
+
+
+        </div>
+
+        <div class="buttonssection">
+
+        <button>Sign in</button>
+        <button>Create account</button>
+
+        </div>
+        
         </div>
         <div class="footer"></div>
 
