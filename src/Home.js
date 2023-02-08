862acb6c9a42becbf58c5236b4aec52cd06a6f49
Cyberstizz <charles.lamb.dev@gmail.com>
2023-02-07 23:33:10 -0500

    added a span tag around the text building smart

--- src/Home.js
+++ src/Home.js
@@ -91,7 +91,7 @@ setAccounts(mytest)
 
             {/* some words for the slogan section*/}
             <div class="sloganSection">
-            Building Smart 
+            <span>Building Smart </span>
             <br/>
                 <span class="wealth">Wealth!</span>
             <br/>
