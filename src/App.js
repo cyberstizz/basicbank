74b38dd512a80a52aef800a2ec3bd91ae3f77dd5
Cyberstizz <charles.lamb.dev@gmail.com>
2023-02-09 23:05:55 -0500

    added create route

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Routes>
          <Route path="/" element={<Home/>} />
          <Route  path="/accounts" element={<Home/>} />
-         <Route />
+         <Route  path="/create" element={<Home/>} />
 
       </Routes>
 
