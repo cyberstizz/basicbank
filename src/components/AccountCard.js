809d36b959be845defd9b460fbf85e0b9c51281c
Cyberstizz <charles.lamb.dev@gmail.com>
2023-02-13 14:49:44 -0500

    added divs for each section on the card

--- src/components/AccountCard.js
+++ src/components/AccountCard.js
@@ -6,7 +6,18 @@ import { useParams } from 'react-router';
 
 const AccountCard = () => {
   return (
-    <div className='cardbackground'>bla</div>
+    <div className='cardbackground'>
+      
+      <div className='accountType'>Checking</div>
+
+      <div className='accountNumber'>47839</div>
+      <div className='goldLine'>goldline</div>
+      <div className='accountBalance'>$12738</div>
+      <div className='accountButtons'>buttons</div>
+
+      
+      
+      </div>
   )
 }
 
