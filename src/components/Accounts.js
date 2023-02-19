11933107761752629d15bba74c1941bad8aea466
Cyberstizz <charles.lamb.dev@gmail.com>
2023-02-19 03:09:12 -0500

    created function to get the csrftoken before my requests

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -11,6 +11,17 @@ const Accounts = () => {
 
   let _csrfToken = null;
 
+  const getCsrfToken = async () => {
+    if (_csrfToken === null) {
+      const response = await fetch(`${API_HOST}/csrf/`, {
+        credentials: 'include',
+      });
+      const data = await response.json();
+      _csrfToken = data.csrfToken;
+    }
+    return _csrfToken;
+  }
+  
 
 
 
