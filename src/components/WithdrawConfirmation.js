1767f4f0312f4361312632fd67b675e8e43bd83d
Cyberstizz <charles.lamb.dev@gmail.com>
2023-02-24 09:36:20 -0500

    added withdraw confirmations boilerplate which is basicaally the withdraw components code with minor changes

--- src/components/WithdrawConfirmation.js
+++ src/components/WithdrawConfirmation.js
@@ -1,10 +1,27 @@
 import {Link} from 'react-router-dom'
 import './WithdrawConfirmation.css'
 
-const WithdrawConfirmation = () => {
+const WithdrawConfirmation = (props) => {
     return (
-        <div>WithdrawConfirmation</div>
-      )
+<div className='fullWithdrawComponent'>
+          
+          
+          <div className='toprow'>
+            <div className='Xout'>&#9747;</div>
+            <div className='withdrawHeader'>Are you sure</div>
+          </div>
+
+          <div className='withdrawSection'>
+            <div className='withdrawSectionHeader'>Deposit Amount</div>
+            <input onChange={props.handleWithdrawAmountChange} type='text' placeholder='$0.00' className='withdrawField'></input>
+          </div>
+          <div className='descriptionSection'>description</div>
+          <button onClick={props.completeWithdraw} className='withdrawButton'>Make Deposit</button>
+
+          
+          
+          
+          </div>      )
 }
 
 
