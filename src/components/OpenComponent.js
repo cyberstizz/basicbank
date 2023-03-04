30876d79196808ac58dae9057f576cb9f6d5e75f
Cyberstizz <charles.lamb.dev@gmail.com>
2023-03-04 15:40:42 -0500

    added boilerplate code for open component

--- src/components/OpenComponent.js
+++ src/components/OpenComponent.js
@@ -3,7 +3,78 @@
 
 const OpenComponent = () => {
     return(
-        <div>Open component</div>
+        <div>
+        <div className="">
+
+        
+        
+        <div className="">
+            <div className="">
+                
+                
+                <div className="">
+        <input placeholder="Name" type="text" className="" onChange={setNameHandler}></input>
+        <div className=""></div>
+        <p className="">Name</p>
+
+                </div>
+
+
+                <div className="">
+        <input placeholder="Password" type="password" className="" onChange={setPasswordHandler}></input>
+        <div className=""></div>
+        <p className="">Password</p>
+                </div>
+
+
+
+                <div className="">
+        <input placeholder="email" type="text" className="" onChange={setEmailHandler}></input>
+        <div className=""></div>
+        <p className="">Email</p>
+                </div>
+       
+       
+        </div>
+
+
+
+
+        <div className="">
+
+
+        <div className="">
+
+        <select placeholder="Account Type" type="text" className="" onChange={setAccountHandler}>
+            <option>Checking</option>
+            <option>Savings</option>
+        </select>
+        <div className=""></div>
+        <p className="">Account type</p>
+
+        </div>
+
+
+        <div className="">
+
+        <input placeholder="Deposit" type="number" min='1' max='1000000' className="" onChange={setDepositHandler}></input>
+        <div className=""></div>
+        <p className="">Deposit</p>
+        </div>
+
+
+        </div>
+        <input type="submit" value="Open Account" className="" onClick={makeAccount}></input>
+
+
+
+        </div>
+
+
+
+
+        </div>
+        </div>
     )
 };
 
