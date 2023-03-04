58a87874f9a47e112bda3ef4fa3ab94216da613e
Cyberstizz <charles.lamb.dev@gmail.com>
2023-03-04 15:45:42 -0500

    created functions for openComponent

--- src/components/OpenComponent.js
+++ src/components/OpenComponent.js
@@ -1,7 +1,133 @@
-
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 const OpenComponent = () => {
+
+    
+   
+   const Create = () => {
+   
+   const navigate = useNavigate();
+   //name, email, account, deposit
+   
+   const [name, setName] = useState('');
+   
+   const [password, setPassword] = useState('');
+   
+   const [email, setEmail] = useState('');
+   
+   const [account, setAccount] = useState('checking');
+   
+   const [deposit, setDeposit] = useState(0);
+   
+   
+   
+   const setNameHandler = (e) => {
+   
+   setName(e.target.value)
+   
+   }
+   
+   const setPasswordHandler = (e) => {
+       
+       setPassword(e.target.value)
+       
+       }
+   
+   
+   const setEmailHandler = (e) => {
+           
+       setEmail(e.target.value)
+           
+   }
+   
+   const setAccountHandler = (e) => {
+   
+               
+       setAccount(e.target.value)
+               
+   };
+   
+   
+   const setDepositHandler = (e) => {
+   
+       
+       setDeposit(e.target.value)
+       
+       };
+   
+       const API_HOST = 'http://localhost:8000';
+   
+       let _csrfToken = null;
+     
+       const getCsrfToken = async () => {
+         if (_csrfToken === null) {
+           const response = await fetch(`${API_HOST}/csrf/`, {
+             credentials: 'include',
+           });
+           const data = await response.json();
+           _csrfToken = data.csrfToken;
+         }
+         return _csrfToken;
+       }
+   
+   
+   
+   
+       const makeAccount = async () => {
+   
+           const allMyStuffObject = {
+               body: {
+               username: name,
+               password: password,
+               email: email,
+               account: account,
+               deposit: deposit
+               }
+           }
+       
+           console.log(`I am the makeAccount function and I have been called,
+           further, you should know that this is the information that you will be sending
+           ${allMyStuffObject.body.account}`)
+   
+   
+   
+   
+   
+           const createAccountCall = await fetch('http://localhost:8000/create', {
+           method: "POST",
+           headers: {
+               'Accept': 'application/json',
+               'Content-Type': 'application/json',
+               'X-CSRFToken': await getCsrfToken()
+           },
+           credentials: 'include',
+           body: JSON.stringify({
+               username: name,
+               password: password,
+               email: email,
+               account: account,
+               deposit: deposit
+           })
+           })
+   
+   console.log(createAccountCall.status)
+   if(createAccountCall.status === 200){
+     alert('success! your account has been succesfulyy created!')
+     navigate('/')
+   }
+   
+           
+   
+   
+   
+       }
+   
+
+
+
+
     return(
         <div>
         <div className="">
@@ -77,5 +203,6 @@ const OpenComponent = () => {
         </div>
     )
 };
+}
 
 export default OpenComponent;
