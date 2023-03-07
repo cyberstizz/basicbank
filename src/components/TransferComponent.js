0d584638ec130734721d309620c3001c929ddd90
Cyberstizz <charles.lamb.dev@gmail.com>
2023-03-07 11:09:02 -0500

    added two divs for top header

--- src/components/TransferComponent.js
+++ src/components/TransferComponent.js
@@ -5,7 +5,11 @@ const TransferComponent = (props) => {
     return (
         <div className='fullTransferComponent'>TransferComponent
         
-        <div className='transferTop'></div>
+        <div className='transferTop'>
+        <div className='Xout' onClick={props.xoutHandler}>x</div>
+        <div className='opennewaccounttextheader'>Open new account</div>
+
+        </div>
         <div className='transferAmountField'></div>
         <div className='transferFromToField'></div>
         <button></button>
