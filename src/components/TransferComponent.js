17002ec72b0058afb41842caa1d12c3c1ea8ae06
Cyberstizz <charles.lamb.dev@gmail.com>
2023-03-07 10:07:15 -0500

    added props to transfer component

--- src/components/TransferComponent.js
+++ src/components/TransferComponent.js
@@ -1,7 +1,7 @@
 import {Link} from 'react-router-dom'
 import './TransferComponent.css'
 
-const TransferComponent = () => {
+const TransferComponent = (props) => {
     return (
         <div className='fullTransferComponent'>TransferComponent</div>
       )
