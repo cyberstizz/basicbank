68719d8b72f1e05924eb0bac8516e3468b7db246
Cyberstizz <charles.lamb.dev@gmail.com>
2023-03-09 15:47:46 -0500

    changed border on transfewrdropdown to black. looks cooler in this situation

--- src/components/TransferComponent.js
+++ src/components/TransferComponent.js
@@ -23,7 +23,7 @@ const TransferComponent = (props) => {
             <div className='transferFromToTop'>From account {transferFrom}</div>
             <div className='transferDecorativeLine'></div>
             <div className='transferFromToBottom'>
-                <div className='transferToWord'>To</div>
+                <div className='transferToWord'>To:</div>
                 <option className='transferDropDown'></option>
 
             </div>
