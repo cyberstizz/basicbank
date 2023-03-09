7bfd0285ccbe6265fc2703c866986270d7040628
Cyberstizz <charles.lamb.dev@gmail.com>
2023-03-09 10:52:06 -0500

    altered the orientation of the html structure

--- src/components/TransferComponent.js
+++ src/components/TransferComponent.js
@@ -13,13 +13,12 @@ const TransferComponent = (props) => {
             <div className='Xout' onClick={props.xoutHandler}>x</div>
             <div className='transferTextheader'>Transfer</div>
         </div>
-        <div className='transferAmountField'>
-        <div className='transferDollarSign'>$</div>
-        <input className='transfer'></input>
-
-
-
+            <div className='transferAmountField'>
+            <div className='transferDollarSign'>$</div>
+            <input className='transfer'></input>
         </div>
+
+
         <div className='transferFromToField'></div>
         <button className='transferButton'>Transfer {transferFrom}</button>
 
