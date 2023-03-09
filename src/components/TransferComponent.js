642c996b8e1ed2d47e47770271588e825d916d94
Cyberstizz <charles.lamb.dev@gmail.com>
2023-03-09 10:39:42 -0500

    aded two divs for the transffer amount field

--- src/components/TransferComponent.js
+++ src/components/TransferComponent.js
@@ -13,7 +13,13 @@ const TransferComponent = (props) => {
             <div className='Xout' onClick={props.xoutHandler}>x</div>
             <div className='transferTextheader'>Transfer</div>
         </div>
-        <div className='transferAmountField'></div>
+        <div className='transferAmountField'>
+        <div></div>
+        <div></div>
+
+
+
+        </div>
         <div className='transferFromToField'></div>
         <button className='transferButton'>Transfer {transferFrom}</button>
 
