29a8b5c26a006af9f31410fe948d1fa12dd66b36
Cyberstizz <charles.lamb.dev@gmail.com>
2023-03-10 10:31:04 -0500

    added make transfer to button so people know what they are clicking

--- src/components/TransferConfirmation.js
+++ src/components/TransferConfirmation.js
@@ -24,7 +24,7 @@ const TransferConfirmation = () => {
           
           
           
-          <button className='transferConfirmationButton'></button>
+          <button className='transferConfirmationButton'> Make Transfer</button>
 
         </div>
       )
